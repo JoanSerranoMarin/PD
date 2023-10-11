e5ba2d01f45e02e8e299712d26d0d73c5a81b35c
Joan Serrano Marín <85892455+JoanSerranoMarin@users.noreply.github.com>
2023-10-11 20:58:13 +0200

    PCA_PD.R

--- PCA_PD.R
+++ PCA_PD.R
@@ -1,4 +1,5 @@
 
+
 install.packages('rgl')
 install.packages('ellipse')
 install.packages("C:/Users/joan.serrano.marin/Downloads/pca3d_0.10.2.tar", repos = NULL, type = "source")
@@ -16,3 +17,15 @@ pca3d(pca, gr=gr)
 pca3d(pca, gr=gr, show.ellipses=TRUE,
       ellipse.ci=0.4, show.plane=FALSE)
 
+
+# Extract the standard deviations of the principal components
+pca_variances <- pca$sdev^2
+
+# Calculate the total variance
+total_variance <- sum(pca_variances)
+
+# Calculate the percentage of variability explained by each component
+percentage_variability_explained <- (pca_variances / total_variance) * 100
+
+# Print the results
+percentage_variability_explained
