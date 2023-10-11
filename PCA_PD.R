9aaacb5d0d89d27ba964fc85d911a3a45a26e485
Joan Serrano Marín <85892455+JoanSerranoMarin@users.noreply.github.com>
2023-10-11 20:07:52 +0200

    PCA_PD.R

--- PCA_PD.R
+++ PCA_PD.R
@@ -3,10 +3,16 @@ install.packages('rgl')
 install.packages('ellipse')
 install.packages("C:/Users/joan.serrano.marin/Downloads/pca3d_0.10.2.tar", repos = NULL, type = "source")
 library(pca3d)
-df <- data.frame(Parkinson_3chosen)
-pca <- prcomp(df[,-1], scale.=TRUE)
+library(rgl)
+library(ellipse)
+df <- data.frame(par_todos_PCA_5)
+df
+pca <- prcomp(na.omit(df[,-1]), scale = TRUE)
+pca
+
 gr <- factor(df[,1])
-pca3d(pca, group=gr)
-pca3d(pca, group=gr, show.ellipses=TRUE,
+gr
+pca3d(pca, gr=gr)
+pca3d(pca, gr=gr, show.ellipses=TRUE,
       ellipse.ci=0.4, show.plane=FALSE)
 
